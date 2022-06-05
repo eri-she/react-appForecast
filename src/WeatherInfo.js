b5dd806a036787456d00b1a8b88a57e9a66545af
eri-she <96880798+eri-she@users.noreply.github.com>
2022-06-05 20:58:52 +0200

    added css

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -13,18 +13,13 @@ export default function WeatherInfo(props) {
       <p className="citySection text-capitalize">{props.data.description}</p>
       <hr />
       <div className="row">
-        <div className="col-sm-2">
-          <WeatherIcon
-            className="icon"
-            code={props.data.icon}
-            size={65}
-            color={"#00B4D8"}
-          />
+        <div className="col-2">
+          <WeatherIcon className="icon" code={props.data.icon} size={65} />
         </div>
-        <div className="col-sm-4">
+        <div className="col-4">
           <WeatherConversion data={props.data.temperature} />
         </div>
-        <div className="col-sm-6">
+        <div className="col-6">
           <p className="wind">Wind: {props.data.wind}%</p>
           <p className="humidity">Humidity: {props.data.humidity}km/h</p>
         </div>
